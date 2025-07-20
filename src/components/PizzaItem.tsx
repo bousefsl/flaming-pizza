7f9775066dd90002ecf05ebd4b21c0986b6ddf15
Scott Bousefield <scott.bousefield.z99@hsgb.co.uk>
2025-07-20 11:42:46 +0100

    pizza list data created

--- src/components/PizzaItem.tsx
+++ src/components/PizzaItem.tsx
@@ -1,20 +1,21 @@
 //Next
-import { PizzaItemProps } from "@/types"
-//Types
 import Image from "next/image"
+//Types
+import { PizzaItemProps } from "@/types"
+//Components
 import { Button } from "./global/CTAs"
 
-export default function PizzaItem({ image, title, content, heatContent, heatImg, buttonText }: PizzaItemProps) {
+export default function PizzaItem({ image, title, content, heatContent, heatImg, blurHash }: PizzaItemProps) {
   return (
     <div className="pizza-card mb-12 grow-0 shrink-0 w-full sm:w-[48%] lg:w-[23%]">
       <div className="flex flex-col h-full">
         <div>
           <div className="mb-4 relative">
-            <Image src={image} alt="" className="rounded-2xl" placeholder="blur" />
+            <Image src={image} width={800} height={600} alt={title} className="rounded-2xl" placeholder="blur" blurDataURL={blurHash} />
             <div className="bg-primary-950 pl-2 pr-4 py-2 rounded-tl-xl rounded-br-xl w-auto absolute right-0 bottom-0">
               <div className="flex flex-row">
                 <div className="mr-2">
-                  <Image src={heatImg} alt={heatContent} />
+                  <Image src={heatImg} alt={heatContent} width={86} height={25} />
                 </div>
                 <p className="text-md text-fp-light-yellow font-bold">{heatContent} heat</p>
               </div>
@@ -26,7 +27,7 @@ export default function PizzaItem({ image, title, content, heatContent, heatImg,
           </div>
         </div>
         <div className="text-center mt-auto">
-          <Button>{buttonText}</Button>
+          <Button>View pizza</Button>
         </div>
       </div>
     </div>
